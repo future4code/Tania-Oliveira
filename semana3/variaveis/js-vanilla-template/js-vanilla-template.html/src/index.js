23d63e66af89e68350b4a9981c74f0128bd36dd3
@elizaoliveira88 <eliza_oliveira88@outlook.com>
2021-04-19 15:52:35 -0300

    variaveis

---
+++ semana3/variaveis/js-vanilla-template/js-vanilla-template.html/src/index.js
@@ -0,0 +1,78 @@
+        // EXERCICIO INTEPR COD
+        // 1-
+        //IMPRIMI 10
+        // IMPRIMI 10, 5
+        //2-
+        //IMPRIMI ERRO, ERRO, ERRO
+
+        
+//A 
+// const nome;
+// console.log(nome)
+
+//B
+// let idade;
+// console.log=(idade)
+
+//C
+//  let nome;
+//  console.log(typeof nome)
+
+// let idade;
+// console.log(typeof idade)
+
+//D
+// Porque só da pra declarar uma variavel sem atribuir valor se for let!
+
+//E
+// const nome = prompt("Qual é seu nome?")
+// const idade= prompt("Qual a sua idade?")
+// console.log(nome)
+// console.log(idade)
+// console.log(typeof nome)
+// console.log(typeof idade)
+//ELAS SÃO DO TIO STRING
+
+//F
+// const nome = prompt("Qual é seu nome?")
+// let idade= prompt("Qual a sua idade?")
+// console.log("Olá" + " "  + nome +  " " + "você tem" + " " + idade + " " + "anos")
+
+
+//2
+// let verboToBePortugues = prompt("Escreva a tradução do verbo to be para o português")
+// let DevSemE = prompt("Escreva a palavra dever sem o 'er', final fica como?")
+// let E = prompt("Escreva como fica a letra E com o acento agudo?")
+// let DeMais = prompt("Escreva por extenso D+")
+// console.log(verboToBePortugues  +  " "  +  DevSemE  +  " "  +  E  +  " "  +  DeMais)
+
+//3
+//A
+// const comidasFavoritas = ["arroz, feijão, batata, pizza, churrasco"]
+// console.log(comidasFavoritas)
+
+//B
+// const comidasFavoritas = ["arroz, feijão, batata, pizza, churrasco"]
+// console.log( "Essas são minhas comidas favoritas" + " " + comidasFavoritas)
+
+//C
+// let comidaPreferidaUsuario = prompt("Escreva sua comida preferida")
+// const comidasFavoritas = ["arroz" + " " + comidaPreferidaUsuario + " " +  "batata, pizza, churrasco"]
+// console.log(comidasFavoritas)
+ 
+
+//4
+
+// const perguntaOndeMora = prompt("Você mora no Brasil? Sim ou não?")
+// console.log("Você mora no Brasil" + " " +  perguntaOndeMora)
+// const perguntaOndeMoraRegiao = prompt("Você mora no Sul do Brasil? Sim ou não?")
+// console.log("Você mora no Brasil" + " " +  perguntaOndeMoraRegiao)
+// const perguntaOndeMoraEstado = prompt("Você mora no em Santa Catarina Brasil? Sim ou não?")
+// console.log("Você mora no Brasil" + " " +  perguntaOndeMoraEstado)
+
+let pergunta = ["Você mora no Brasil?", "Você mora no Sul do Brasil?", "Você mora no em Santa Catarina Brasil?"]
+let resposta = [true, false]
+console.log(pergunta[0], resposta[0])
+console.log(pergunta[1], resposta[0])
+console.log(pergunta[2], resposta[1])
+
