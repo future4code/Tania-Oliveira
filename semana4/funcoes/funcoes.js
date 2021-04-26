cd9d42fb3315ef8f689495923afabd1dbd53440c
@elizaoliveira88 <eliza_oliveira88@outlook.com>
2021-04-26 16:06:09 -0300

    funcoes

---
+++ semana4/funcoes/funcoes.js
@@ -0,0 +1,39 @@
+//----------------------------Exercícios de interpretação de código
+//-------------------EXERCÍCIO 1.
+//A- 10 E 50.
+//B-Não apareceria nada.
+
+//-----------------EXERCÍCIO 2
+
+//A- Seria impresso Darvas e Caio.
+//B- Seria impresso Amanda e Caio.
+
+
+//--------------------EXERCÍCIO 3
+
+//A-
+//Ela verifica cada elemento do array se é par, e adiciona no final do array a multiplicação e garda no array final.
+//Um nome melhor seria arrayInicial.
+
+//--------------------------EXERCÍCIO 4
+ //a.
+// function quemSou(){
+//     console.log('Meu nome é Tania Oliveira, sou estudante moro em Caxias do Sul')
+// }
+// quemSou()
+
+//b.
+// function quemSou(nome, idade, cidade,) {
+//  console.log(`Eu sou ${nome} tenho ${idade} e nasci na cidade de ${cidade}`)
+    
+// }
+
+// quemSou('tania', 33, 'Caxias do Sul')
+
+//--------------------------EXERCÍCIO 5
+// function somando(numeroUm, numeroDois) {
+//     const soma = numeroUm + numeroDois
+//     return soma
+// }
+// const resultado = somando(2, 3)
+// console.log(resultado)
