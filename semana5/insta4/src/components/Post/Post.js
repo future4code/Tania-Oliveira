93a74a15272219dd041f5b75ca5cc465115d4a08
@elizaoliveira88 <eliza_oliveira88@outlook.com>
2021-05-05 16:18:48 -0300

    insta4

---
+++ semana5/insta4/src/components/Post/Post.js
@@ -0,0 +1,114 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import {IconeComContador} from '../IconeComContador/IconeComContador'
+
+import iconeCoracaoBranco from '../../img/favorite-white.svg'
+import iconeCoracaoPreto from '../../img/favorite.svg'
+import iconeComentario from '../../img/comment_icon.svg'
+import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
+
+const PostContainer = styled.div`
+  border: 1px solid gray;
+  width: 300px;
+  margin-bottom: 10px;
+`
+
+const PostHeader = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+`
+
+const PostFooter = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  justify-content: space-between;
+`
+
+const UserPhoto = styled.img`
+  height: 30px;
+  width: 30px;
+  margin-right: 10px;
+  border-radius: 50%;
+`
+
+const PostPhoto = styled.img`
+  width: 100%;
+`
+//pus export aqui
+export class Post extends React.Component {
+  state = {
+    curtido: false,
+    numeroCurtidas: 3,
+    comentando: false,
+    numeroComentarios: 6
+  }
+
+  onClickCurtida = () => {
+    // console.log('Curtiu!')
+    
+    this.setState({
+      curtido: false,
+      curtido: this.state.onClickCurtida})
+  }
+
+  onClickComentario = () => {
+    this.setState({
+      comentando: !this.state.comentando
+    })
+  }
+
+  aoEnviarComentario = () => {
+    this.setState({
+      comentando: false,
+      numeroComentarios: this.state.numeroComentarios + 1
+    })
+  }
+
+  render() {
+    let iconeCurtida
+
+    if(this.state.curtido) {
+      iconeCurtida = iconeCoracaoPreto
+    } else {
+      iconeCurtida = iconeCoracaoBranco
+    }
+
+    let componenteComentario
+
+    if(this.state.comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>
+    }
+
+    return <PostContainer>
+      <PostHeader>
+        <UserPhoto src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <p>{this.props.nomeUsuario}</p>
+      </PostHeader>
+
+      <PostPhoto src={this.props.fotoPost} alt={'Imagem do post'}/>
+
+      <PostFooter>
+        <IconeComContador
+          icone={iconeCurtida}
+          onClickIcone={this.state.onClickCurtida}
+          valorContador={this.state.numeroCurtidas}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={this.state.onClickComentario}
+          valorContador={this.state.numeroComentarios}
+          
+        />
+      </PostFooter>
+      {componenteComentario}
+    </PostContainer>
+  }
+}
+
+export default Post
