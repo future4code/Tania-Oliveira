f104a4d64f9c2cf6702251ac5fe6926e9188ed13
@elizaoliveira88 <eliza_oliveira88@outlook.com>
2021-05-24 08:12:36 -0300

    labefy

---
+++ labefy/labefy/src/AppStyled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components"
+
+export const AppContainer = styled.div`
+display: grid;
+grid-template-columns: 1fr 3fr;
+background-color: aliceblue;
+`
---
+++ labefy/labefy/src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default class Home extends React.Component {
+  render() {
+    console.log(this.props)
+    return (
+      <div>
+        <h1>Labefy home</h1>
+      </div>
+    )
+  }
+}
