78d852e851f6fcec70475383e5131e2836508c23
elizaoliveira88 <eliza_oliveira88@outlook.com>
2021-08-19 16:17:11 -0300

    arrumarGenerate

---
+++ projeto_labook/src/controller/UserController.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+import { UserBusiness } from "../business/user/UserBusiness";
+
+
+export class UserController {
+  async signup(req: Request, res: Response) {
+    try {
+      const { email, name, password } = req.body
+
+      const userBusiness = new UserBusiness()
+      const token = await userBusiness.signup({ email, name, password })
+
+      res.send({
+        message: "Usuário cadastrado com sucesso",
+        token
+      })
+    } catch (error) {
+      res.status(400).send(error.message)
+    }
+  }
+
+  async login(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body
+
+      const userBusiness = new UserBusiness()
+      const token = await userBusiness.login({ email, password })
+
+      res.send({
+        message: "Usuário logado com sucesso",
+        token
+      })
+    } catch (error) {
+      res.status(error.statusCode || 400).send(error.message)
+    }
+  }
+}
---
+++ projeto_labook/src/services/TokenManager.ts
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken'
+
+
+export default interface authenticationData{
+  id:string
+}
+
+export class TokenManager {
+
+  generateToken(
+    payload: authenticationData
+  ): string {
+    return jwt.sign(
+       payload,
+       process.env.JWT_KEY as string,
+       {
+          expiresIn: process.env.JWT_EXPIRES_IN
+       }
+    )
+  }
+  
+  getTokenData(
+    token: string
+  ): authenticationData {
+    const result: any = jwt.verify(
+       token,
+       process.env.JWT_KEY as string
+    )
+  
+    return { id: result.id, }
+  }
+}
